8c7b2e8d8b1c58f4e395868797b7055b813324f3
Kousar Raza <49365112+kousarraza@users.noreply.github.com>
2024-05-23 20:46:40 +0500

    Update main.py

--- Examples/Move Beeper-2/main.py
+++ Examples/Move Beeper-2/main.py
@@ -1,4 +1,18 @@
 
-move()
-pick_beeper()
-move()
+"""
+This is a worked example. This code is starter code; you should edit and run it to
+solve the problem. You can click the blue show solution button on the left to see
+the answer if you get too stuck or want to check your work!
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+   
+  move()
+  pick_beeper()
+  move()
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
