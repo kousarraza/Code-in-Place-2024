5b33bbc9b74fe59214ced3d51287a6937a59077e
KousarRaza <kousarraza69@gmail.com>
2024-05-27 19:50:15 +0500

    ..

---
+++ Challenges/Warmp/main.py
@@ -0,0 +1,21 @@
+from karel.stanfordkarel import *
+
+# File: warmup.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which currently just has one
+# Command. Add two more commands to make karel: move(),
+# pick_beeper(), move()
+def main():
+    move()
+    pick_beeper()
+    move()
+    # add your code here
+   
+   
+   
+
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
